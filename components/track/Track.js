46520d6b02e475f6346925f53e7541708378133d
aryankumar897 <82169659+aryankumar897@users.noreply.github.com>
2024-08-15 19:51:33 +0530

    Initial commit

--- components/track/Track.js
+++ components/track/Track.js
@@ -1,10 +1,5 @@
 // components/DeviceDetails.js
 
-
-"use client"
-
-// components/DeviceDetails.js
-
 import { useEffect, useState } from 'react';
 import {
     Table,
@@ -29,74 +24,74 @@ const DeviceDetails = () => {
         timezone: '',
     });
 
-    const navigator = navigator
-
     useEffect(() => {
-        const fetchDeviceDetails = async () => {
-            try {
-                // Collect client-side data
-                const screenWidth = window.screen.width;
-                const screenHeight = window.screen.height;
-                const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        if (typeof window !== 'undefined' && typeof navigator !== 'undefined') {
+            const fetchDeviceDetails = async () => {
+                try {
+                    // Collect client-side data
+                    const screenWidth = window.screen.width;
+                    const screenHeight = window.screen.height;
+                    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-                // Detect browser and OS
-                const userAgent = navigator.userAgent;
-                console.log(userAgent);
-                let browserName = 'Unknown';
-                let browserVersion = 'Unknown';
-                let os = 'Unknown';
-                let deviceType = 'Unknown';
+                    // Detect browser and OS
+                    const userAgent = navigator.userAgent;
+                    console.log(userAgent);
+                    let browserName = 'Unknown';
+                    let browserVersion = 'Unknown';
+                    let os = 'Unknown';
+                    let deviceType = 'Unknown';
 
-                if (/Android/.test(userAgent)) os = 'Android';
-                else if (/iPhone|iPad|iPod/.test(userAgent)) os = 'iOS';
-                else if (/Windows NT/.test(userAgent)) os = 'Windows';
-                else if (/Mac OS X/.test(userAgent)) os = 'MacOS';
+                    if (/Android/.test(userAgent)) os = 'Android';
+                    else if (/iPhone|iPad|iPod/.test(userAgent)) os = 'iOS';
+                    else if (/Windows NT/.test(userAgent)) os = 'Windows';
+                    else if (/Mac OS X/.test(userAgent)) os = 'MacOS';
 
-                if (/Chrome/.test(userAgent)) {
-                    browserName = 'Chrome';
-                    browserVersion = userAgent.match(/Chrome\/(\d+\.\d+)/)?.[1];
-                } else if (/Safari/.test(userAgent)) {
-                    browserName = 'Safari';
-                    browserVersion = userAgent.match(/Version\/(\d+\.\d+)/)?.[1];
-                } else if (/Firefox/.test(userAgent)) {
-                    browserName = 'Firefox';
-                    browserVersion = userAgent.match(/Firefox\/(\d+\.\d+)/)?.[1];
-                } else if (/MSIE|Trident/.test(userAgent)) {
-                    browserName = 'Internet Explorer';
-                    browserVersion = userAgent.match(/(MSIE \d+\.\d+|rv:\d+\.\d+)/)?.[1];
-                } else if (/Edge/.test(userAgent)) {
-                    browserName = 'Edge';
-                    browserVersion = userAgent.match(/Edge\/(\d+\.\d+)/)?.[1];
-                } else if (/Brave/.test(userAgent)) {
-                    browserName = 'Brave';
-                    browserVersion = userAgent.match(/Brave\/(\d+\.\d+)/)?.[1];
+                    if (/Chrome/.test(userAgent)) {
+                        browserName = 'Chrome';
+                        browserVersion = userAgent.match(/Chrome\/(\d+\.\d+)/)?.[1];
+                    } else if (/Safari/.test(userAgent)) {
+                        browserName = 'Safari';
+                        browserVersion = userAgent.match(/Version\/(\d+\.\d+)/)?.[1];
+                    } else if (/Firefox/.test(userAgent)) {
+                        browserName = 'Firefox';
+                        browserVersion = userAgent.match(/Firefox\/(\d+\.\d+)/)?.[1];
+                    } else if (/MSIE|Trident/.test(userAgent)) {
+                        browserName = 'Internet Explorer';
+                        browserVersion = userAgent.match(/(MSIE \d+\.\d+|rv:\d+\.\d+)/)?.[1];
+                    } else if (/Edge/.test(userAgent)) {
+                        browserName = 'Edge';
+                        browserVersion = userAgent.match(/Edge\/(\d+\.\d+)/)?.[1];
+                    } else if (/Brave/.test(userAgent)) {
+                        browserName = 'Brave';
+                        browserVersion = userAgent.match(/Brave\/(\d+\.\d+)/)?.[1];
+                    }
+
+                    if (screenWidth <= 768) deviceType = 'Mobile';
+                    else if (screenWidth <= 1024) deviceType = 'Tablet';
+                    else deviceType = 'Desktop';
+
+                    setDeviceDetails({
+                        screenWidth,
+                        screenHeight,
+                        timezone,
+                        browserName,
+                        browserVersion,
+                        os,
+                        deviceType,
+                    });
+                } catch (error) {
+                    console.error('Error fetching device details:', error);
                 }
+            };
 
-                if (screenWidth <= 768) deviceType = 'Mobile';
-                else if (screenWidth <= 1024) deviceType = 'Tablet';
-                else deviceType = 'Desktop';
-
-                setDeviceDetails({
-                    screenWidth,
-                    screenHeight,
-                    timezone,
-                    browserName,
-                    browserVersion,
-                    os,
-                    deviceType,
-                });
-            } catch (error) {
-                console.error('Error fetching device details:', error);
-            }
-        };
-
-        fetchDeviceDetails();
+            fetchDeviceDetails();
+        }
     }, []);
 
     return (
-        <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" >
+        <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
             <Box width="100%" maxWidth="lg" p={3} boxShadow={3} borderRadius={8}>
-                <Typography variant="h4" gutterBottom align="center" style={{ marginBottom: 20,color:"white" }}>
+                <Typography variant="h4" gutterBottom align="center" style={{ marginBottom: 20, color: "white" }}>
                     Device Details
                 </Typography>
                 <TableContainer component={Paper}>
@@ -108,18 +103,6 @@ const DeviceDetails = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            <TableRow hover>
-                                <TableCell><strong>User Agent</strong></TableCell>
-                                <TableCell>{navigator.userAgent}</TableCell>
-                            </TableRow>
-                            <TableRow hover>
-                                <TableCell><strong>Platform</strong></TableCell>
-                                <TableCell>{navigator.platform}</TableCell>
-                            </TableRow>
-                            <TableRow hover>
-                                <TableCell><strong>Language</strong></TableCell>
-                                <TableCell>{navigator.language}</TableCell>
-                            </TableRow>
                             <TableRow hover>
                                 <TableCell><strong>Screen Width</strong></TableCell>
                                 <TableCell>{deviceDetails.screenWidth}px</TableCell>
