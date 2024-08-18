06a92c6d5741344628107a96f5942f2caa70c3a9
aryankumar897 <82169659+aryankumar897@users.noreply.github.com>
2024-08-18 14:21:39 +0530

    Initial commit

--- config.js
+++ config.js
@@ -1,25 +1,34 @@
-const DB_URI="mongodb+srv://gahavay527:cvyuMm21CrYgACKS@cluster0.6lx9n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const DB_URI = "mongodb+srv://gahavay527:cvyuMm21CrYgACKS@cluster0.6lx9n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 //"mongodb://127.0.0.1:27017/auth"
 
 //const API = "http://localhost:3000/api"
 
- const API="https://authx-mu.vercel.app/api"
+const API = "https://authx-mu.vercel.app/api"
 
-// GOOGLE_CLIENT_ID = your - google - client - id
-// GOOGLE_CLIENT_SECRET = your - google - client - secret
-// GITHUB_CLIENT_ID = your - github - client - id
-// GITHUB_CLIENT_SECRET = your - github - client - secret
-// FACEBOOK_CLIENT_ID = your - facebook - client - id
-// FACEBOOK_CLIENT_SECRET = your - facebook - client - secret
-// DISCORD_CLIENT_ID = your - discord - client - id
-// DISCORD_CLIENT_SECRET = your - discord - client - secret
-// TWITTER_CLIENT_ID = your - twitter - client - id
-// TWITTER_CLIENT_SECRET = your - twitter - client - secret
-// NEXTAUTH_URL = http://localhost:3000
 
-const DOMAIN ="https://authx-mu.vercel.app"
+
+GOOGLE_CLIENT_ID = '920302764991-cgbf07sbnq7osgh97palmmaovtnnpm5p.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = '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'
+GITHUB_CLIENT_ID = 'Ov23liXKVU9dKkNh1cMs'
+GITHUB_CLIENT_SECRET = '01f84c5791d96b6e50305ee5a21a70a3fe91c1dc'
+// FACEBOOK_CLIENT_ID=''
+// FACEBOOK_CLIENT_SECRET=''
+DISCORD_CLIENT_ID = '1270802720441368596'
+DISCORD_CLIENT_SECRET = 'zk1xRzvjYRB3srOnN-FuMr4CpqwM3i7z'
+TWITTER_CLIENT_ID = 'D3PkJPuBpg12raWn1o8JVendG'
+TWITTER_CLIENT_SECRET = 'XHABN1Xc3A9BZM3Qiymt13kx5BK4mk2hIiCYwJglHEtfaaLiWM'
+AUTH0_CLIENT_ID = 'QJ00nL4KshdwtFBbRF4vyY2k6Ylfpz0g'
+AUTH0_CLIENT_SECRET = 'FQKOBh0qQON0MJIeOZYathBu30o17N707s12oGkS55lakhIbH7OzsJQ4HjKWc2-O'
+AUTH0_ISSUER = 'https://dev-ya4w05rwdbd25t32.us.auth0.com'
+
+
+
+
+NEXTAUTH_URL = "https://authx-mu.vercel.app"
+
+const DOMAIN = "https://authx-mu.vercel.app"
 
 
 //const DOMAIN = "http://localhost:3000"
@@ -28,7 +37,7 @@ const DOMAIN ="https://authx-mu.vercel.app"
 
 
 
-CLOUDINARY_CLOUD_NAME = "dxhzy8hjp",
+     CLOUDINARY_CLOUD_NAME = "dxhzy8hjp",
     CLOUDINARY_API_KEY = "732193517963228",
     CLOUDINARY_API_SECRET = "laHrHWwDnUr_wYP7jdsz8Z7XYyE",
 
